c7b1cba504cfad335f00c36a73c034b8582d462a
EliaGorokhovsky <eliapgorokhovsky@gmail.com>
2020-03-07 00:04:26 -0800

    Add api mapping for serializer

--- backend/assassins/views.py
+++ backend/assassins/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AssassinSerializer
+from .models import Assassin
 
-# Create your views here.
+class AssassinView(viewsets.ModelViewSet):
+    serializer_class = AssassinSerializer
+    queryset = Assassin.objects.all()
