61f7b9242dda340283076d4cefd3bff0bf28b237
Antonio Caceres <antonio@antonio-caceres.com>
2020-03-07 11:44:23 -0800

    Added the Avery logo and added a header bar. Updated gitignore. Also, Comic Sans is the superior font.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,48 @@
+// Important Things //
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+// CSS Stylesheets //
 import './index.css';
 import './assets/css/nav.css'
 
+// Pages //
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import QRGenerator from './pages/QRGenerator';
 import QRScanner from './pages/QRScanner';
 
-ReactDOM.render(
-  <Router>
-      <div class="navbar">
-          <aside>
-            <ul>
-              <li><Link to={`/login`}>Login</Link></li>
-              <li><Link to={`/`}>Dashboard</Link></li>
-              <li><Link to={`/qr`}>Your QR</Link></li>
-              <li><Link to={`/scan`}>Scan a code</Link></li>
-            </ul>
-          </aside>
+// Images //
+import AveryLogo from './assets/images/avery.svg'
 
-          <main>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/qr" component={QRGenerator} />
-            <Route path="/scan" component={QRScanner} />
-          </main>
-      </div>
-  </Router>,
-  document.getElementById('root')
+ReactDOM.render(
+    <Router>
+        <div>
+            <div class="navbar">
+                <aside>
+                    <ul>
+                        <li>
+                            <img width="50" height="50" src={AveryLogo} alt="Avery Logo :("/>
+                            <h1>ss</h1>
+                            <img width="50" height="50" src={AveryLogo} alt="Avery Logo :("/>
+                            <h1>ssery</h1>
+                        </li>
+                        <li><Link to={`/login`}>Login</Link></li>
+                        <li><Link to={`/`}>Dashboard</Link></li>
+                        <li><Link to={`/qr`}>Your QR</Link></li>
+                        <li><Link to={`/scan`}>QR Scan</Link></li>
+                    </ul>
+                </aside>
+
+                <main>
+                    <Route exact path="/" component={Dashboard}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/qr" component={QRGenerator}/>
+                    <Route path="/scan" component={QRScanner}/>
+                </main>
+            </div>
+        </div>
+    </Router>,
+    document.getElementById('root')
 );
