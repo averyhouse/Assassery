9a60461944a1ce2081725af1424099e6fb865407
Antonio Caceres <antonio@antonio-caceres.com>
2020-03-07 18:17:02 -0800

    Update Logo, navbar, and overall website formatting.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 // Important Things //
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 // CSS Stylesheets //
 import './index.css';
-import './assets/css/nav.css'
+import './assets/css/nav.css';
 
 // Pages //
 import Login from './pages/Login';
@@ -14,34 +14,33 @@ import QRGenerator from './pages/QRGenerator';
 import QRScanner from './pages/QRScanner';
 
 // Images //
-import AveryLogo from './assets/images/avery.svg'
+import AveryLogo from './assets/images/avery.svg';
 
 ReactDOM.render(
     <Router>
-        <div>
-            <div class="navbar">
-                <aside>
-                    <ul>
-                        <li>
-                            <img width="50" height="50" src={AveryLogo} alt="Avery Logo :("/>
-                            <h1>ss</h1>
-                            <img width="50" height="50" src={AveryLogo} alt="Avery Logo :("/>
-                            <h1>ssery</h1>
-                        </li>
-                        <li><Link to={`/login`}>Login</Link></li>
-                        <li><Link to={`/`}>Dashboard</Link></li>
-                        <li><Link to={`/qr`}>Your QR</Link></li>
-                        <li><Link to={`/scan`}>QR Scan</Link></li>
-                    </ul>
-                </aside>
+        <div class="navbar">
+            <Link to={`/`}>
+                <logo>
+                    <img width="75" height="75" src={AveryLogo} alt="Avery Logo :("/>
+                    <h1>ss</h1>
+                    <img width="75" height="75" src={AveryLogo} alt="Avery Logo :("/>
+                    <h1>ssery</h1>
+                </logo>
+            </Link>
+            <ul>
+                <li><Link to={`/qr`}>Your QR Code</Link></li>
+                <li><Link to={`/scan`}>QR Scanner</Link></li>
+                <li><Link to={`/login`}>Login</Link></li>
+            </ul>
+        </div>
 
-                <main>
-                    <Route exact path="/" component={Dashboard}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/qr" component={QRGenerator}/>
-                    <Route path="/scan" component={QRScanner}/>
-                </main>
-            </div>
+        <div class="main">
+            <main>
+                <Route exact path="/" component={Dashboard}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/qr" component={QRGenerator}/>
+                <Route path="/scan" component={QRScanner}/>
+            </main>
         </div>
     </Router>,
     document.getElementById('root')
