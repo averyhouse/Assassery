5a7aa306a316305a7b97c45b64fb45d00ff06816
EliaGorokhovsky <eliapgorokhovsky@gmail.com>
2022-01-03 19:48:13 -0800

    Add STATIC_ROOT and STATICFILES_DIRS to settings.

--- backend/settings.py
+++ backend/settings.py
@@ -143,6 +143,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 # we whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = [
