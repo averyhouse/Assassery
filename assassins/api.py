5ed90db8e6e632da93612266a929a71aa1180e7e
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-04-26 23:29:59 -0700

    change time delta

--- assassins/api.py
+++ assassins/api.py
@@ -165,7 +165,7 @@ class DashboardAPI(generics.GenericAPIView):
     def get(self, request):
         def time(kill):
             t = kill.timestamp
-            new_time = t + timedelta(hours=5)
+            new_time = t - timedelta(hours=7)
             return new_time.strftime("%m/%d/%Y, %H:%M:%S")
 
         kills = KillFeed.objects.order_by('-timestamp')
